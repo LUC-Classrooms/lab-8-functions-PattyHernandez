2d1cc610e0699f05423b4031b309cbd70c5c99d6
Patty Hernandez <156938710+PattyHernandez@users.noreply.github.com>
2024-02-28 17:10:23 +0000

    still working

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function draw() {
 
   //this is a call to myShape()
   myShape(width / 2, height / 2, 1);
+  myShape(width/3, height/3,2)
+  myShape(width * 2/3, height * 2/3, 0.5)
   // myShape() takes three arguments
   // arg1: horizontal location
   // arg2: vertical location
@@ -16,13 +18,18 @@ function draw() {
   
 }
 
-function myShape(x, y, s) {
+function myShape(x, y, s,a ) {
   // make this function more interesting
   push(); // make a separate layer
   translate(x, y); // move the origin point
   scale(s);
-  
-  ellipse(0, 0, w, h); // simple ellipse at the translated origin (0,0)
-  
+ rotate(a)
+  ellipse(0, 0, 50, 50); // simple ellipse at the translated origin (0,0)
+  fill(255)
+  ellipse(-10,-10,10); 
+  ellipse( 10,-10, 10);
+  fill(255,0,0)
+  ellipse(10,-10,3);
+  ellipse( 10, -10,3); 
   pop(); // dispose of the layer
 }
