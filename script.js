bc6529cf71573815ef120f7839ea8ba2c967a0c3
Patty Hernandez <156938710+PattyHernandez@users.noreply.github.com>
2024-03-04 23:45:44 +0000

    lab 8 complete

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function draw() {
   background(200);
 
   //this is a call to myShape()
-  myShape(width / 2, height / 2, 1);
-  myShape(width/3, height/3,2)
-  myShape(width * 2/3, height * 2/3, 0.5)
+  myShape(width/2, height /2, 2);
+  myShape(width/3, height/3,2.5)
+  myShape(width * 2/3, height * 2/3, 1.5)
   // myShape() takes three arguments
   // arg1: horizontal location
   // arg2: vertical location
@@ -23,13 +23,16 @@ function myShape(x, y, s,a ) {
   push(); // make a separate layer
   translate(x, y); // move the origin point
   scale(s);
- rotate(a)
   ellipse(0, 0, 50, 50); // simple ellipse at the translated origin (0,0)
   fill(255)
+  fill("black")
   ellipse(-10,-10,10); 
   ellipse( 10,-10, 10);
-  fill(255,0,0)
-  ellipse(10,-10,3);
-  ellipse( 10, -10,3); 
+  ellipse(10,-10,10);
+  fill("red")
+  ellipse( 10, -10,5);
+  ellipse(-10,-10,5); 
+  ellipse(-10,-10,5);
+  
   pop(); // dispose of the layer
 }
